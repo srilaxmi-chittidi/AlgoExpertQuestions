e60682a1e97dc9fa8c2be8f86230d83f931d5948
laxmi.chittidi <laxmi.chittidi@gmail.com>
2023-08-19 15:50:05 -0700

    Added One Edit prg

--- AEStringEasyNMediumQuestions/src/com/prgs/strings/medium/OneEdit.java
+++ AEStringEasyNMediumQuestions/src/com/prgs/strings/medium/OneEdit.java
@@ -3,8 +3,47 @@ package com.prgs.strings.medium;
 public class OneEdit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(isOneEdit("ab","a")); /* expected true , got false */
+		System.out.println(isOneEdit("helo","hello"));
 
 	}
-
+	/** One test case is failed*/
+	public static boolean isOneEdit(String stringOne,String stringTwo) {
+		
+		if(stringOne.equals(stringTwo)) return true;
+		if(Math.abs(stringOne.length()-stringTwo.length()) > 1) return false;
+		
+		StringBuilder sOne = new StringBuilder(stringOne);
+		StringBuilder sTwo = new StringBuilder(stringTwo);
+		
+		Boolean isEdited = false;
+		
+		for(int i=0 ; i<Math.min(stringOne.length(), stringTwo.length()) ; i++) {
+			if(sOne.charAt(i) == sTwo.charAt(i)) continue;
+			
+			if(sOne.length() == sTwo.length()) {
+				sOne.setCharAt(i, sTwo.charAt(i));
+				isEdited = true;
+			}else if(sOne.length() > sTwo.length()) {
+				sTwo.insert(i, sOne.charAt(i));
+				isEdited = true;
+			}else if(sOne.length() < sTwo.length()) {
+				sOne.insert(i, sTwo.charAt(i));
+				isEdited = true;
+			}
+			break;
+		}
+		
+		if(!isEdited) {
+			if(sOne.length() > sTwo.length()) {
+				sOne.append(sTwo.charAt(sTwo.length()-1));
+			}
+			if(sOne.length() < sTwo.length()) {
+				sTwo.append(sOne.charAt(sOne.length()-1));
+			}
+		}
+		
+		return sOne.toString().equals(sTwo.toString());
+		
+	}
 }
